a155746bdd788bfb670929fccaab0520a1c342cf
devRok <gktjdfhr2@naver.com>
2025-05-17 23:18:46 +0900

    🚀 Initial commit

---
+++ src/test/java/com/unity/tribe/TribeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unity.tribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TribeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
