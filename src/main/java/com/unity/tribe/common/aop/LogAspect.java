43cb09dcd974571b252f4db19d3da7e4f5122d01
하성록 <gktjdfhr2@naver.com>
2025-05-22 00:12:09 +0900

    📝 로그 AOP 클래스 추가

---
+++ src/main/java/com/unity/tribe/common/aop/LogAspect.java
@@ -0,0 +1,9 @@
+package com.unity.tribe.common.aop;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+// TODO API 로깅 AOP 구현
+@RestControllerAdvice
+public class LogAspect {
+}
