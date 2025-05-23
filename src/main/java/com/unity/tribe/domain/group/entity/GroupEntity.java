4daef16638dd6b6e1814886897ae8e8ab8535852
nullisdefined <jaeuu.dev@gmail.com>
2025-05-23 23:29:28 +0900

    ♻️ groupId 타입을 String(ULID)에서 Long으로 변경

--- src/main/java/com/unity/tribe/domain/group/entity/GroupEntity.java
+++ src/main/java/com/unity/tribe/domain/group/entity/GroupEntity.java
@@ -3,214 +3,151 @@ package com.unity.tribe.domain.group.entity;
 import java.sql.Timestamp;
 import java.util.Objects;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "group", schema = "tribe", catalog = "")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GroupEntity {
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    public enum GroupStatus {
+        WAITING, ONGOING, FINISHED, DISBANDED
+    }
+
     @Id
     @Column(name = "group_id")
     private String groupId;
+
     @Basic
-    @Column(name = "host_id")
+    @Column(name = "host_id", nullable = false)
     private String hostId;
+
     @Basic
-    @Column(name = "category_id")
-    private int categoryId;
+    @Column(name = "category_id", nullable = false)
+    private Long categoryId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", insertable = false, updatable = false)
+    private GroupCategory category;
+
     @Basic
-    @Column(name = "title")
+    @Column(name = "title", nullable = false)
     private String title;
+
     @Basic
     @Column(name = "description")
     private String description;
+
     @Basic
     @Column(name = "thumbnail")
     private String thumbnail;
+
     @Basic
     @Column(name = "group_type")
     private String groupType;
+
     @Basic
     @Column(name = "meeting_type")
     private String meetingType;
+
     @Basic
     @Column(name = "location_address")
     private String locationAddress;
+
     @Basic
-    @Column(name = "participants")
-    private int participants;
+    @Column(name = "location_latitude")
+    private String locationLatitude;
+
+    @Basic
+    @Column(name = "location_longitude")
+    private String locationLongitude;
+
+    @Basic
+    @Column(name = "participants", nullable = false)
+    private Integer participants;
+
     @Basic
     @Column(name = "gender_restriction")
     private String genderRestriction;
+
     @Basic
     @Column(name = "min_age")
-    private int minAge;
+    private Integer minAge;
+
     @Basic
     @Column(name = "max_age")
-    private int maxAge;
+    private Integer maxAge;
+
     @Basic
-    @Column(name = "status")
-    private String status;
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private GroupStatus status;
+
     @Basic
-    @Column(name = "created_at")
+    @Column(name = "created_at", nullable = false)
     private Timestamp createdAt;
+
     @Basic
-    @Column(name = "updated_at")
+    @Column(name = "updated_at", nullable = false)
     private Timestamp updatedAt;
+
     @Basic
     @Column(name = "expired_at")
     private Timestamp expiredAt;
 
-    public String getGroupId() {
-        return groupId;
-    }
-
-    public void setGroupId(String groupId) {
-        this.groupId = groupId;
-    }
-
-    public String getHostId() {
-        return hostId;
-    }
-
-    public void setHostId(String hostId) {
-        this.hostId = hostId;
-    }
-
-    public int getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(int categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getThumbnail() {
-        return thumbnail;
-    }
-
-    public void setThumbnail(String thumbnail) {
-        this.thumbnail = thumbnail;
-    }
-
-    public String getGroupType() {
-        return groupType;
-    }
-
-    public void setGroupType(String groupType) {
-        this.groupType = groupType;
-    }
-
-    public String getMeetingType() {
-        return meetingType;
-    }
-
-    public void setMeetingType(String meetingType) {
-        this.meetingType = meetingType;
-    }
-
-    public String getLocationAddress() {
-        return locationAddress;
-    }
-
-    public void setLocationAddress(String locationAddress) {
-        this.locationAddress = locationAddress;
-    }
-
-    public int getParticipants() {
-        return participants;
-    }
-
-    public void setParticipants(int participants) {
-        this.participants = participants;
-    }
-
-    public String getGenderRestriction() {
-        return genderRestriction;
-    }
-
-    public void setGenderRestriction(String genderRestriction) {
-        this.genderRestriction = genderRestriction;
-    }
-
-    public int getMinAge() {
-        return minAge;
-    }
-
-    public void setMinAge(int minAge) {
-        this.minAge = minAge;
-    }
-
-    public int getMaxAge() {
-        return maxAge;
-    }
-
-    public void setMaxAge(int maxAge) {
-        this.maxAge = maxAge;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Timestamp getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Timestamp createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Timestamp getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Timestamp updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public Timestamp getExpiredAt() {
-        return expiredAt;
-    }
-
-    public void setExpiredAt(Timestamp expiredAt) {
-        this.expiredAt = expiredAt;
-    }
-
     @Override
     public boolean equals(Object o) {
-        if (this == o) { 
-            return true; 
+        if (this == o) {
+            return true;
         }
         if (o == null || getClass() != o.getClass()) {
-            return false; 
+            return false;
         }
         GroupEntity that = (GroupEntity) o;
-        return categoryId == that.categoryId && participants == that.participants && minAge == that.minAge && maxAge == that.maxAge && Objects.equals(groupId, that.groupId) && Objects.equals(hostId, that.hostId) && Objects.equals(title, that.title) && Objects.equals(description, that.description) && Objects.equals(thumbnail, that.thumbnail) && Objects.equals(groupType, that.groupType) && Objects.equals(meetingType, that.meetingType) && Objects.equals(locationAddress, that.locationAddress) && Objects.equals(genderRestriction, that.genderRestriction) && Objects.equals(status, that.status) && Objects.equals(createdAt, that.createdAt) && Objects.equals(updatedAt, that.updatedAt) && Objects.equals(expiredAt, that.expiredAt);
+        return Objects.equals(groupId, that.groupId)
+                && Objects.equals(hostId, that.hostId)
+                && Objects.equals(categoryId, that.categoryId)
+                && Objects.equals(title, that.title)
+                && Objects.equals(description, that.description)
+                && Objects.equals(thumbnail, that.thumbnail)
+                && Objects.equals(groupType, that.groupType)
+                && Objects.equals(meetingType, that.meetingType)
+                && Objects.equals(locationAddress, that.locationAddress)
+                && Objects.equals(locationLatitude, that.locationLatitude)
+                && Objects.equals(locationLongitude, that.locationLongitude)
+                && Objects.equals(participants, that.participants)
+                && Objects.equals(genderRestriction, that.genderRestriction)
+                && Objects.equals(minAge, that.minAge)
+                && Objects.equals(maxAge, that.maxAge)
+                && Objects.equals(status, that.status)
+                && Objects.equals(createdAt, that.createdAt)
+                && Objects.equals(updatedAt, that.updatedAt)
+                && Objects.equals(expiredAt, that.expiredAt);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(groupId, hostId, categoryId, title, description, thumbnail, groupType, meetingType, locationAddress, participants, genderRestriction, minAge, maxAge, status, createdAt, updatedAt, expiredAt);
+        return Objects.hash(groupId, hostId, categoryId, title, description, thumbnail,
+                groupType, meetingType, locationAddress, locationLatitude,
+                locationLongitude, participants, genderRestriction, minAge,
+                maxAge, status, createdAt, updatedAt, expiredAt);
     }
 }
