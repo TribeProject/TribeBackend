3dfb233bc267e6c15bb613834bfcd2df634cc732
devRok <gktjdfhr2@naver.com>
2025-05-29 20:51:20 +0900

    🔧 enum 및 Util 클래스 추가

--- src/main/java/com/unity/tribe/common/util/DateUtil.java
+++ src/main/java/com/unity/tribe/common/util/DateUtil.java
@@ -1,4 +1,48 @@
 package com.unity.tribe.common.util;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import org.springframework.util.StringUtils;
+
 public class DateUtil {
+
+    public static LocalDateTime getLocalDateTimeFromString(String stringDate) {
+        if (!StringUtils.hasText(stringDate)) {
+            throw new RuntimeException("문자열이 비어있습니다.");
+        }
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        LocalDate localDate = LocalDate.parse(stringDate, formatter);
+        return localDate.atStartOfDay();
+    }
+
+    public static String getLocalDateTimeToString(LocalDateTime localDateTime, String pattern) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        return localDateTime.format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    public static String getTodayLocalDateTimeToString(String pattern) {
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        return getLocalDateTimeToString(localDateTime, pattern);
+    }
+
+    public static LocalDate getLocalDateFromString(String stringDate) {
+        if (!StringUtils.hasText(stringDate)) {
+            throw new RuntimeException("문자열이 비어있습니다.");
+        }
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        return LocalDate.parse(stringDate, formatter);
+
+    }
+
+    public static String getStringFromDate(Date date, String type) {
+        SimpleDateFormat formatter = new SimpleDateFormat(type);
+        return formatter.format(date);
+    }
 }
--- src/main/java/com/unity/tribe/common/util/StringUtil.java
+++ src/main/java/com/unity/tribe/common/util/StringUtil.java
@@ -1,4 +1,31 @@
 package com.unity.tribe.common.util;
 
+import org.springframework.util.StringUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class StringUtil {
+
+    public static String maskString(String targetString, int index) {
+        if(!StringUtils.hasText(targetString) || targetString.length() < index) {
+            return targetString;
+        }
+
+        StringBuilder maskedUserId = new StringBuilder(targetString);
+        for (int i = index - 1; i < targetString.length(); i++) {
+            maskedUserId.setCharAt(i, '*'); // 3번째 자리부터 '*'로 대체
+        }
+
+        return maskedUserId.toString();
+    }
+
+    public static String ObjectToString(Object obj) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            return objectMapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
